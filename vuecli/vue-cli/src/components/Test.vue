67e3f4d1978155e96d81b8fb8ba1752bddcd1bcf
fengchuanyu <fenchuanyu@163.com>
2021-05-15 10:36:08 +0800

    admin

---
+++ vuecli/vue-cli/src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+       <h1>{{str}}</h1> 
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Test",
+    data() {
+        return {
+            str:"这是一个测试页面"
+        }
+    },
+}
+</script>
+
+<style scoped>
+h1{
+    color: red;
+}
+</style>
